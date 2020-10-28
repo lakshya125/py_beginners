db5af196155d30326896ce86dd9364cafabb3bda
Shreyas-Gupta-21 <62789950+Shreyas-Gupta-21@users.noreply.github.com>
2020-10-29 01:07:52 +0530

    Update fibonacci_series.py

--- fibonacci_series.py
+++ fibonacci_series.py
@@ -1,9 +1,10 @@
 #Python programme to print fibonacci series
-
-n = int(input("Enter number of terms to be printed:"))
+n = int(input("Enter number of terms to be printed: "))
 first = 0
 second = 1
-for x in range(0, n ) :
+print(first)
+print(second)
+for x in range(0, n-2 ) :
   third = first + second
-  print(third)
   first , second =second,third
+  print(third)
