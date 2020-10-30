ecd687a705b10978ca8dea188062b4671c0381ed
Diamond Purvis <Diamond.JPurvis@ibm.com>
2020-10-30 17:30:26 -0500

    print a calculation

---
+++ radius.py
@@ -0,0 +1,13 @@
+# Find the area of a circle
+# Print the area
+
+print("The area of a circle with a radius of 5 is ");
+print(3.1416 * 5 * 5);
+
+
+# Find the area of a half circle
+# Print the area
+
+print("The area of a a half circle with a radius of 5 is ");
+print((3.1416 * 5 * 5) / 2);
+
