1b16fad95f735e878d8a2498b6bd5f76f495ef6c
Anish Agarwal <34007888+thisisanish@users.noreply.github.com>
2020-10-31 18:29:51 +0530

    Update bitapi.py

--- bitapi.py
+++ bitapi.py
@@ -2,6 +2,7 @@ import requests as req
 import time
 now=time.ctime()
 
+# Fetches BitCoin's Current Price
 bit = req.get('https://api.coindesk.com/v1/bpi/currentprice.json')
 
 print("1 Bitcoin is worth $" + bit.json()['bpi']['USD']['rate'] + " as of "+ now)
