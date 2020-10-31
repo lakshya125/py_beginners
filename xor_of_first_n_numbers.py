9f17b6545a68f0905ce453b7315aa45f79112dd2
AmanDeep-21 <amandeepsmile21@gmail.com>
2020-10-31 17:57:04 +0530

    added program for finding xor of first n numbers

---
+++ xor_of_first_n_numbers.py
@@ -0,0 +1,13 @@
+# Program to find xor of first n numbers
+
+# Taking input
+n = int(input())
+
+if n % 4 == 0:
+    print(n)
+elif n % 4 == 1:
+    print('1')
+elif n % 4 == 2:
+    print(n + 1)
+else:
+    print('0')
