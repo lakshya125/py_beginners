4343e810bca0abae3af6d996b170ccbcd5636852
Lakshya Dhariwal <lakshyadhariwal9@gmail.com>
2021-04-05 18:17:49 +0530

    Update HanoiTowers.py
    Add description comment to file

--- HanoiTowers.py
+++ HanoiTowers.py
@@ -1,3 +1,9 @@
+
+'''The Tower of Hanoi is a mathematical game or puzzle. It consists of three rods and a number of disks of different sizes, which can slide onto any rod.
+This is a recursive implementation of this problem in Python.
+'''
+
+
 def TowerOfHanoi(n, src, dest, aux):
     if n == 1:
         print("Move disk 1 from source", src, "to destination", dest)
@@ -5,6 +11,7 @@ def TowerOfHanoi(n, src, dest, aux):
     TowerOfHanoi(n - 1, src, aux, dest)
     print("Move disk", n, "from source", src, "to destination", dest)
     TowerOfHanoi(n - 1, aux, dest, src)
+
 
 if __name__ == "__main__":
     n = 4
